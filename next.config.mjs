45789aeb0e2a271b80fde0ea6da898321f59a0a0
Francesco Zanchetta <151642330+zjncoo@users.noreply.github.com>
2026-08-18 13:20:56 +0200

    Add PWA support to Next.js configuration
    Initialize Next.js configuration with PWA support.

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
