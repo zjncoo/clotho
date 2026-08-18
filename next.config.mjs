bda82c6c512bd4f7371ebf2b743d509930d896b2
Francesco Zanchetta <151642330+zjncoo@users.noreply.github.com>
2026-08-18 13:47:44 +0200

    Remove webpack configuration from next.config.mjs
    Removed custom webpack configuration from next.config.mjs.

--- next.config.mjs
+++ next.config.mjs
@@ -15,19 +15,6 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-
-    if (isServer) {
-      config.externals = [...(config.externals || []), 'onnxruntime-web'];
-    }
-
-    return config;
-  },
 };
 
 export default withPWA(nextConfig);
