4d462410bc52dfda52db8924b140398e3c30e1ee
Francesco Zanchetta <151642330+zjncoo@users.noreply.github.com>
2026-08-18 13:22:35 +0200

    Add color palette with RGB values

---
+++ utils/colorPalette.ts
@@ -0,0 +1,17 @@
+export const COLOR_PALETTE: { name: string; rgb: [number, number, number] }[] = [
+  { name: 'Nero', rgb: [20, 20, 20] },
+  { name: 'Bianco', rgb: [245, 245, 245] },
+  { name: 'Grigio Antracite', rgb: [70, 70, 70] },
+  { name: 'Grigio Chiaro', rgb: [190, 190, 190] },
+  { name: 'Blu Navy', rgb: [20, 35, 60] },
+  { name: 'Blu Aviazione', rgb: [70, 130, 180] },
+  { name: 'Celeste', rgb: [175, 215, 235] },
+  { name: 'Verde Salvia', rgb: [138, 154, 134] },
+  { name: 'Verde Militare', rgb: [75, 83, 32] },
+  { name: 'Beige / Sabbia', rgb: [215, 195, 160] },
+  { name: 'Marrone Testa di Moro', rgb: [60, 40, 30] },
+  { name: 'Cammello', rgb: [193, 154, 107] },
+  { name: 'Bordeaux', rgb: [110, 20, 40] },
+  { name: 'Rosso', rgb: [200, 30, 30] },
+  { name: 'Panna / Ecrù', rgb: [240, 235, 220] }
+];
