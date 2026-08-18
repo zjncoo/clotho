ec8795bc0c420ed8d7ca47b82ec6fedfa4739607
Francesco Zanchetta <151642330+zjncoo@users.noreply.github.com>
2026-08-18 13:41:11 +0200

    Change icon type from PNG to SVG in manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: '/icons/icon-192x192.png',
         sizes: '192x192',
-        type: 'image/png',
+        type: 'image/svg+xml',
         purpose: 'maskable',
       },
       {
         src: '/icons/icon-512x512.png',
         sizes: '512x512',
-        type: 'image/png',
+        type: 'image/svg+xml',
         purpose: 'any',
       },
     ],
