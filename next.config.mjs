e70aa20fccea14bedabe427c1540a363e7c676ad
Francesco Zanchetta <151642330+zjncoo@users.noreply.github.com>
2026-08-18 13:44:48 +0200

    Enhance webpack configuration for experiments
    Add WebAssembly and layers support in webpack config.

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,19 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'onnxruntime-web'];
+    }
+
+    return config;
+  },
 };
 
 export default withPWA(nextConfig);
